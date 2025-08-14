b7ba06a609663d4bf29653df42f123ab6e535b8c
Yuri Romanov <newakk2242@gmail.com>
2025-08-14 16:05:29 +0200

    Убрал фикстуру collection_five_books, оставил только collection для создания пустого BooksCollector

--- conftest.py
+++ conftest.py
@@ -4,22 +4,10 @@ from main import BooksCollector
 
 @pytest.fixture
 def collection():
-    collection = BooksCollector()
-    return collection
+    """Создаёт пустой экземпляр коллектора книг"""
+    return BooksCollector()
 
-"""Для корректного отображения аргументов в параметризированном тесте"""
-def pytest_make_parametrize_id(val): 
+
+def pytest_make_parametrize_id(val):
+    """Для корректного отображения аргументов в параметризированном тесте"""
     return repr(val)
-
-@pytest.fixture
-def collection_five_books(collection):
-    collect = collection
-    books = ['Властелин колец', 'Король лев', 'Чужой', 'Сон в летнюю ночь', 'Молчание ягнят']
-    genre = ['Фантастика', 'Мультфильмы', 'Ужасы', 'Комедии', 'Детективы']
-    for i in range(5):
-        collect.add_new_book(books[i])
-
-    for i in range(5):
-        collect.set_book_genre(books[i], genre[i])
-
-    return collect
